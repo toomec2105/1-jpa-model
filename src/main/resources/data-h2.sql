1785339d5ea3e66ea3015a81fd7cbb5c0e7714d2
toomec2105 <tomec2105@interia.pl>
2020-07-23 18:44:03 +0200

    fill banners

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,3 @@
+insert into springuser values(1,'Janusz');
+insert into springuser values(2,'Steve');
+insert into springuser values(3,'Deckard');
---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,6 @@
+insert into springuser values(1,'Janusz');
+insert into springuser values(2,'Steve');
+insert into springuser values(3,'Deckard');
+insert into springuser values(4,'Dovakin');
+insert into springuser values(5,'John');
+insert into springuser values(6,'Lara');
