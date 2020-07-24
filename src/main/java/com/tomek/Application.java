d7830d77127577153cab11b3370303e04f438b32
toomec2105 <tomec2105@interia.pl>
2020-07-24 18:58:09 +0200

    add loggers

--- src/main/java/com/tomek/Application.java
+++ src/main/java/com/tomek/Application.java
@@ -1,13 +1,18 @@
 package com.tomek;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
+	
 	public static void main(String[] args) {
+		logger.info("----------------------> Starting Spring"); 
 		SpringApplication.run(Application.class, args);
+		logger.info("----------------------> Ending Spring's run()"); 
 	}
 
 }
